2a1d992c1e5ffe2ea708bad3b60b6a17ef51b462
anhphamn8706-pixel <anhphamn8706@gmail.com>
2026-06-15 12:55:47 +0700

    Update appointment.py

---
+++ src/entities/appointment.py
@@ -0,0 +1,23 @@
+from structures.linked_list import LinkedList
+
+class Appointment:
+    def __init__(
+        self,
+        appointment_id,
+        patient_id,
+        schedule_id,
+        status="Đã đặt"
+    ):
+        self.id = appointment_id
+        self.patient_id = patient_id
+        self.schedule_id = schedule_id
+        self.status = status
+
+    def __str__(self):
+        return (
+            f"Appointment("
+            f"ID: {self.id}, "
+            f"Patient_ID: {self.patient_id}, "
+            f"Schedule_ID: {self.schedule_id}, "
+            f"Status: {self.status})"
+        )
