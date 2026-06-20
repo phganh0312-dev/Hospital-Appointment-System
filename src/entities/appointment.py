654b00c66781343514e06bd47aa3dbe418c857ec
VŨ PHƯƠNG ANH <phganh0312@gmail.com>
2026-06-20 09:30:56 +0700

    Add files via upload

--- src/entities/appointment.py
+++ src/entities/appointment.py
@@ -6,12 +6,14 @@ class Appointment:
         appointment_id,
         patient_id,
         schedule_id,
-        status="Đã đặt"
+        status="Đã đặt",
+        payment_status="Chưa thanh toán"
     ):
         self.id = appointment_id
         self.patient_id = patient_id
         self.schedule_id = schedule_id
         self.status = status
+        self.payment_status = payment_status
 
     def __str__(self):
         return (
--- src/entities/patient.py
+++ src/entities/patient.py
@@ -1,14 +1,17 @@
 from structures.linked_list import LinkedList
 
 class Patient:
-    def __init__(self, patient_id, cccd, full_name, phone, province, ward, detailed_address):
+    def __init__(self, patient_id, cccd, full_name, phone, email, province, ward, detailed_address, bhyt_code=None, bhyt_expiry=None):
         self.id = patient_id            
         self.cccd = cccd                
         self.full_name = full_name
         self.phone = phone
+        self.email = email
         self.province = province
         self.ward = ward
         self.detailed_address = detailed_address
+        self.bhyt_code = bhyt_code
+        self.bhyt_expiry = bhyt_expiry
 
     def __str__(self):
-        return f"Patient(ID: {self.id}, CCCD: {self.cccd}, Name: {self.full_name})"
+        return f"Patient(ID: {self.id}, CCCD: {self.cccd}, Name: {self.full_name}, BHYT: {self.bhyt_code})"
