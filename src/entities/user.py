0da3080604de4a047f530f4a750c86446772fe4d
VŨ PHƯƠNG ANH <phganh0312@gmail.com>
2026-06-20 09:22:42 +0700

    Add files via upload

---
+++ src/entities/user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, user_id, phone, email, password, role, linked_id=None):
+        self.user_id = user_id
+        self.phone = phone
+        self.email = email
+        self.password = password
+        self.role = role
+        self.linked_id = linked_id
+
+    def __str__(self):
+        return f"{self.user_id}|{self.phone}|{self.email}|{self.password}|{self.role}|{self.linked_id}"
